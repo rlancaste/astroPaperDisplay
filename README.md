81960f39c51438961696a829dfe86adc73bc5dd0
Robert Lancaster <rlancaste@gmail.com>
2019-08-30 16:31:27 -0400

    Updating readme and install script

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # astroPaperDisplay
 Updates an EPaper Display with information about the AstroPhotography on an SBC
+
+Did you ever have your Astro Imaging Device in an observing field and wonder if it has connected to a network
+and if so, what its hostname or IP address happens to be?  
+
+This set of scripts is based upon a WaveShare EPaper/EInk Module/Hat for a raspberry pi and is meant to get it to display
+its name, network IP address, currently connected Network, and hostname.  That way you know that you can connect and how.
+
+The EInk Module(s) it is based upon:  
+https://www.waveshare.com/wiki/2.13inch_e-Paper_HAT_(B)
+https://www.waveshare.com/wiki/2.13inch_e-Paper_HAT_(C)
+
+User Manual:
+https://www.waveshare.com/w/upload/4/4a/2.13inch-e-paper-hat-b-user-manual-en.pdf
+
+To get this set up, you would need to:
+1. Buy a Module
+2. Connect it as described in the manual, either as a HAT or with wires in a module configuration
+3. Turn on your Raspberry pi, open Terminal, and clone this repo in your home directory
+	git clone http://www.github.com/rlancaste/astroPaperDisplay
+4. Run the installAstroPaper.sh script to install it all.'
+	cd ~/astroPaperDisplay
+	sudo ./installAstroPaper.sh
+
--- installAstroPaper.sh
+++ installAstroPaper.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+function display
+{
+    echo ""
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "~ $*"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo ""
+    
+    # This will display the message in the title bar (Note that the PS1 variable needs to be changed too--see below)
+    echo -en "\033]0;astroPaperDisplay-$*\a"
+}
+
+display "Welcome to the AstroPaperDisplay Installation Script."
+
+display "This will update, install the scripts and software requirements to support the WaveShare EInk 2.13 inch e paper display for astrophotography. Be sure to read the script first to see what it does and to customize it."
+
+if [ "$(whoami)" != "root" ]; then
+	display "Please run this script with sudo due to the fact that it must do a number of sudo tasks.  Exiting now."
+	exit 1
+fi
+
+read -p "Are you ready to proceed (y/n)? " proceed
+
+if [ "$proceed" != "y" ]
+then
+	exit
+fi
+
+# This changes the UserPrompt for the Setup Script (Necessary to make the messages display in the title bar)
+PS1='astroPaperDisplay~$ '
 
 #Installs the requirements for the ePaper Display
-
+display "Installing Software Requirements"
 sudo apt -y install python3-pip python-PIL
 pip3 install spidev
 pip3 install RPi.GPIO
@@ -9,11 +41,13 @@ pip3 install Pillow
 
 
 # Enables SPI
+display "Enabling SPI for Raspberry Configuration"
 if [ -f /boot/config.txt ]
 then
 	sed -i "s/dtparam=spi=off/dtparam=spi=on/g" /boot/config.txt
 fi
 
+display "Creating and installing display update script."
 # This script will run anytime a network comes up to update the display
 ##################
 sudo cat > /etc/NetworkManager/dispatcher.d/updateDisplay.sh <<- EOF
@@ -26,18 +60,19 @@ if [ "$status" == "up" ]
 then
 	if [ "$interface" == "eth0" ]
 	then
-		/usr/bin/python3 /home/pi/astroPaperDisplay/displayEthStatus.py
+		/usr/bin/python3 $DIR/displayEthStatus.py
 	fi
 
 	if [ "$interface" == "wlan0" ]
 	then
-		/usr/bin/python3 /home/pi/astroPaperDisplay/displayWifiStatus.py
+		/usr/bin/python3 $DIR/displayWifiStatus.py
 	fi
 fi
 EOF
 ##################
 sudo chmod a+x /etc/NetworkManager/dispatcher.d/updateDisplay.sh
 
+display "Creating and starting system shutdown monitor service for shutdown script"
 # This service monitors shutdown and when it detects it, clears the display.
 ##################
 sudo cat > /etc/systemd/system/ShutdownDisplay.service <<- EOF
@@ -48,7 +83,7 @@ Description=Shutdown the EInk Display
 Type=oneshot
 RemainAfterExit=true
 ExecStart=/bin/true
-ExecStop=/usr/bin/python3 /home/pi/astroPaperDisplay/shutdownDisplay.py
+ExecStop=/usr/bin/python3 $DIR/shutdownDisplay.py
 
 [Install]
 WantedBy=multi-user.target
